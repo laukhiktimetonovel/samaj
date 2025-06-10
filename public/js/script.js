9a3f44a5bd507440f5811dca08290238c6e9d88c
Laukhik Patidar <timetonovel2016@gmail.com>
2025-06-10 18:32:45 +0530

    add application feature

--- public/js/script.js
+++ public/js/script.js
@@ -49,3 +49,11 @@
 //     link.classList.add('page-active');
 //   }
 // })
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('{{ asset("service-worker.js") }}')
+      .then(registration => console.log('Service Worker registered:', registration))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+    <meta name="theme-color" content="#B3541E" />
     <title>તળપદા કોળી પટેલ સમાજ</title>
     <link rel="icon" href="{{ asset('images/logo.png') }}">
+    
     {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
     <link href="https://fonts.googleapis.com/css2?family=Anek+Gujarati:wght@100..800&display=swap" rel="stylesheet">
 
