c6ddd88a3d1a1ef8ad3d5e2a0d96312bdbaca047
Laukhik Patidar <timetonovel2016@gmail.com>
2025-06-27 18:25:28 +0530

    solve issue of worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,20 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => cache.addAll(['/']))
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/css/style.css', // Adjust paths to your assets
+        '/js/script.js',
+        '/images/icon-192x192.png'
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
